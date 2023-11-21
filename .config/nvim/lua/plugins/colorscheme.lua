afe0221ed7f3a670119909896d0823c69b9cd5dc
louiskim <louiskim.levu@gmail.com>
2023-11-21 23:53:57 +0800

    rosepine

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add rosepine
+  { "rose-pine/neovim", name = "rose-pine" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
